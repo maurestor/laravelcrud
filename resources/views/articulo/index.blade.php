04e893bd41f924f17fec94a126066e5bee2b7460
mau_restor <restor@gmail.com>
2022-09-28 00:04:02 -0500

    laravel crud with datatables

--- resources/views/articulo/index.blade.php
+++ resources/views/articulo/index.blade.php
@@ -1,44 +1,61 @@
 @extends('layouts.plantillabase')
 
+@section('datatables-css')
+    <link href='https://cdn.datatables.net/1.12.1/css/dataTables.bootstrap5.min.css' rel="stylesheet">
+@endsection
+
 @section('contenido')
 
 <h1>Vista INDEX</h1>
 
 <a href="/articulos/create" class="btn btn-primary">Crear</a>
 
-    <div class="table-responsive">
-        <table class="table table-dark table-striped mt-4">
-            <thead>
-                <tr>
-                    <th scope="col">ID</th>
-                    <th scope="col">Codigo</th>
-                    <th scope="col">Descripcion</th>
-                    <th scope="col">Cantidad</th>
-                    <th scope="col">Precio</th>
-                    <th scope="col">Acciones</th>
-                    
+
+    <table id="articulos" class="table table-dark table-striped mt-4" style="width:100%;">
+        <thead>
+            <tr>
+                <th scope="col">ID</th>
+                <th scope="col">Codigo</th>
+                <th scope="col">Descripcion</th>
+                <th scope="col">Cantidad</th>
+                <th scope="col">Precio</th>
+                <th scope="col">Acciones</th>
+                
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($articulos as $articulo)
+            <tr>
+                <td>{{$articulo->id}}</td>
+                <td>{{$articulo->codigo}}</td>
+                <td>{{$articulo->descripcion}}</td>
+                    <td>{{$articulo->cantidad}}</td>
+                    <td>{{$articulo->precio}}</td>
+                    <td>
+                        <form action="{{ route('articulos.destroy', $articulo->id)}}" method="POST">
+                            <a href="/articulos/{{$articulo->id}}/edit" class="btn btn-info" >Editar</a>
+                            @csrf
+                            @method("DELETE")
+                            <button class="btn btn-danger">Borrar</button>
+                        </form>
+                    </td>
                 </tr>
-            </thead>
-            <tbody>
-                @foreach ($articulos as $articulo)
-                    <tr>
-                        <td>{{$articulo->id}}</td>
-                        <td>{{$articulo->codigo}}</td>
-                        <td>{{$articulo->descripcion}}</td>
-                        <td>{{$articulo->cantidad}}</td>
-                        <td>{{$articulo->precio}}</td>
-                        <td>
-                            <form action="{{ route('articulos.destroy', $articulo->id)}}" method="POST">
-                                <a href="/articulos/{{$articulo->id}}/edit" class="btn btn-info" >Editar</a>
-                                @csrf
-                                @method("DELETE")
-                                <button class="btn btn-danger">Borrar</button>
-                            </form>
-                        </td>
-                    </tr>
-                @endforeach
-            </tbody>
-        </table>
-    </div>
+            @endforeach
+        </tbody>
+    </table>
+
+@section('datatables-js')
+    <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+    <script src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.12.1/js/dataTables.bootstrap5.min.js"></script>
+    <script>
+        $(document).ready(function () {
+            $('#articulos').DataTable({
+                "lengthMenu": [[5, 10, 50, -1], [5, 10, 50, "All"]]
+            });
+        });
+    </script>
+@endsection
+
 
 @endsection
--- resources/views/layouts/plantillabase.blade.php
+++ resources/views/layouts/plantillabase.blade.php
@@ -4,6 +4,9 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    
+    @yield('datatables-css')
+    
     <title>CRUD</title>
 </head>
   <body>
@@ -14,5 +17,8 @@
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+    
+    @yield('datatables-js')
+  
   </body>
 </html>
